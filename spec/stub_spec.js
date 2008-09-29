f7bff551696c6086b233edf52ee2b236f110ec19
Trotter Cashion <cashion@gmail.com>
2008-09-29 15:52:08 -0400

    Add Screw.Stub.reset to reset stubs

--- spec/stub_spec.js
+++ spec/stub_spec.js
@@ -10,11 +10,31 @@ Screw.Unit(function() {
             obj.stub("pizza");
             expect(obj.pizza()).to(equal, null);
         });
-
+        
         it("accepts a return value", function() {
             var ret = "hello";
             obj.stub("pizza").and_return(ret);
             expect(obj.pizza()).to(equal, ret);
+        });
+
+        it("stubs over stubs", function() {
+            obj.stub("pizza").and_return("cheese");
+            obj.stub("pizza").and_return("sausage");
+            expect(obj.pizza()).to(equal, "sausage");
+        });
+
+        it("resets stubs", function() {
+            obj = (function() {
+              self = {};
+              var private = 'hello';
+              self.pizza = function() { return private };
+              return self;
+            })();
+            var expected = obj.pizza();
+
+            obj.stub("pizza");
+            Screw.Stub.reset();
+            expect(obj.pizza()).to(equal, expected);
         });
     });
 });
