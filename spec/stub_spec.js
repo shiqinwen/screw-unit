2d263536814cf6b90f5ea1c92bfa657e29957ebb
Trotter Cashion <cashion@gmail.com>
2008-09-30 07:59:45 -0400

    Wrap all stub methods in own description

--- spec/stub_spec.js
+++ spec/stub_spec.js
@@ -6,35 +6,37 @@ Screw.Unit(function() {
             obj = {};
         });
 
-        it("returns null for stubbed methods", function() {
-            obj.stub("pizza");
-            expect(obj.pizza()).to(equal, null);
-        });
-        
-        it("accepts a return value", function() {
-            var ret = "hello";
-            obj.stub("pizza").andReturn(ret);
-            expect(obj.pizza()).to(equal, ret);
-        });
+        describe(".stub", function() {
+            it("returns null for stubbed methods", function() {
+                obj.stub("pizza");
+                expect(obj.pizza()).to(equal, null);
+            });
+            
+            it("accepts a return value", function() {
+                var ret = "hello";
+                obj.stub("pizza").andReturn(ret);
+                expect(obj.pizza()).to(equal, ret);
+            });
 
-        it("stubs over stubs", function() {
-            obj.stub("pizza").andReturn("cheese");
-            obj.stub("pizza").andReturn("sausage");
-            expect(obj.pizza()).to(equal, "sausage");
-        });
+            it("stubs over stubs", function() {
+                obj.stub("pizza").andReturn("cheese");
+                obj.stub("pizza").andReturn("sausage");
+                expect(obj.pizza()).to(equal, "sausage");
+            });
 
-        it("resets stubs", function() {
-            obj = (function() {
-              self = {};
-              var private = 'hello';
-              self.pizza = function() { return private; };
-              return self;
-            })();
-            var expected = obj.pizza();
+            it("resets stubs", function() {
+                obj = (function() {
+                  self = {};
+                  var private = 'hello';
+                  self.pizza = function() { return private; };
+                  return self;
+                })();
+                var expected = obj.pizza();
 
-            obj.stub("pizza");
-            Screw.Stub.reset();
-            expect(obj.pizza()).to(equal, expected);
+                obj.stub("pizza");
+                Screw.Stub.reset();
+                expect(obj.pizza()).to(equal, expected);
+            });
         });
     });
 });
